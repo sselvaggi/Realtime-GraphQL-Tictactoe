b0be001cdcf46cb987624103fb909d0ddb82e379
Sergio Selvaggi <sergio.selvaggi@ordatic.com>
2020-09-14 22:16:09 +0200

    installed graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,41 @@
+import { GraphQLModule } from '@nestjs/graphql';
 import { Module } from '@nestjs/common';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+import { PingPongResolvers } from './ping-pong.resolvers';
+import * as Redis from 'ioredis';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      playground: true,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+      },
+      installSubscriptionHandlers: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    PingPongResolvers,
+    {
+      provide: 'PUB_SUB',
+      useFactory: () => {
+        const options = {
+          host: process.env.REDIS_URI,
+          port: process.env.REDIS_PORT,
+        };
+
+        return new RedisPubSub({
+          publisher: new Redis(options),
+          subscriber: new Redis(options),
+        });
+      },
+    },
+  ],
 })
 export class AppModule {}
